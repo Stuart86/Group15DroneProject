5baab4114387364cfc14c5b5baf610eebb4cbd01
lasse <lassejensen213@gmail.com>
2018-06-11 00:46:45 +0200

    Testing with color contrast

--- src/controller/Controller.py
+++ src/controller/Controller.py
@@ -46,6 +46,29 @@ class Controller(object):
             if (frameCounter % 2 == 0):
 
                 continue
+            #-----Converting image to LAB Color model----------------------------------- 
+            lab= cv2.cvtColor(frame, cv2.COLOR_BGR2LAB)
+            cv2.imshow("lab",lab)
+            
+            #-----Splitting the LAB image to different channels-------------------------
+            l, a, b = cv2.split(lab)
+            cv2.imshow('l_channel', l)
+            cv2.imshow('a_channel', a)
+            cv2.imshow('b_channel', b)
+            
+            #-----Applying CLAHE to L-channel-------------------------------------------
+            clahe = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8,8))
+            cl = clahe.apply(l)
+            cv2.imshow('CLAHE output', cl)
+            
+            #-----Merge the CLAHE enhanced L-channel with the a and b channel-----------
+            limg = cv2.merge((cl,a,b))
+            cv2.imshow('limg', limg)
+            
+            #-----Converting image from LAB Color model to RGB model--------------------
+            final = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+            cv2.imshow('final', final)
+
             result = findAndReadQR(frame)
             if(len(result) > 0):
                 for i in result:
